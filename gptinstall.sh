df4997bda8c5e392c81193f52e7b5cd77f275c48
Abderrahim Zabir <bahimzabir@gmail.com>
2023-04-08 02:00:42 +0000

    removing the old chatgpt folder

--- gptinstall.sh
+++ gptinstall.sh
@@ -1,11 +1,13 @@
 if [ $(cat ~/.zshrc | grep "node" | wc -l) -gt 0 ]; then
 	echo 'export PATH="$PATH:$HOME/chatgpt"' >> ~/.zshrc
 	cd $HOME
+	rm -rf chatgpt
 	git clone https://github.com/bahimzabir/terminal_chatgpt.git chatgpt && chmod 777 ./chatgpt/chatgpt && rm -rf ./chatgpt/.git ./chatgpt/.gitignore
 	echo '\n--------------\ninstallation done:\n Usage: chatgbt <question>'
 elif [ -n "$BASH_VERSION" ]; then
 	echo 'export PATH="$PATH:$HOME/chatgpt"' >> ~/.bashrc
 	cd $HOME
+	rm -rf chatgpt
 	git clone https://github.com/bahimzabir/terminal_chatgpt.git chatgpt && chmod 777 ./chatgpt/chatgpt && rm -rf ./chatgpt/.git ./chatgpt/.gitignore
 	echo '\n--------------\ninstallation done:\n Usage: chatgbt <question>'
 else
