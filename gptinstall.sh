4b7cb60fce1bdd59d0ce174ebd7355e9a46816df
Abderrahim Zabir <bahimzabir@gmail.com>
2023-04-08 18:38:51 +0000

    b to p

--- gptinstall.sh
+++ gptinstall.sh
@@ -3,4 +3,4 @@
 	git clone https://github.com/bahimzabir/terminal_chatgpt.git chatgpt && chmod 777 ./chatgpt/chatgpt && rm -rf ./chatgpt/.git ./chatgpt/.gitignore
 	echo 'export PATH="$PATH:$HOME/chatgpt"' >> ~/.zshrc
 	echo 'export PATH="$PATH:$HOME/chatgpt"' >> ~/.bashrc
-	echo '\n--------------\ninstallation done:\n Usage: chatgbt <question>'
+	echo '\n--------------\ninstallation done:\n Usage: chatgpt <question>'
